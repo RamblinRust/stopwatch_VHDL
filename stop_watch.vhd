60a2f7e0f772f3cd7ee08426354eaba4717468c3
RamblinRust <58799293+RamblinRust@users.noreply.github.com>
2020-09-26 22:32:49 -0500

    Update stop_watch.vhd
    removed the decimal points from the seven seg components.
    Added the if-statement for the r_started rtl issue I was having.

--- stop_watch.vhd
+++ stop_watch.vhd
@@ -11,11 +11,11 @@ entity stop_watch is
 		  i_50MHz : in std_logic;--50MHz crystal from board
 		  i_st	 : in std_logic;--start/stop button
 		  i_clr	 : in std_logic;--clear button
-		  o_top0, o_rtop0, o_rbot0, o_bot0, o_lbot0, o_ltop0, o_mid0, o_dp0 : out std_logic; --output pins for 7seg 0
-		  o_top1, o_rtop1, o_rbot1, o_bot1, o_lbot1, o_ltop1, o_mid1, o_dp1 : out std_logic; --output pins for 7seg 1
-		  o_top2, o_rtop2, o_rbot2, o_bot2, o_lbot2, o_ltop2, o_mid2, o_dp2 : out std_logic; --output pins for 7seg 2
-		  o_top3, o_rtop3, o_rbot3, o_bot3, o_lbot3, o_ltop3, o_mid3, o_dp3 : out std_logic; --output pins for 7seg 3
-		  o_top4, o_rtop4, o_rbot4, o_bot4, o_lbot4, o_ltop4, o_mid4, o_dp4 : out std_logic  --output pins for 7seg 4
+		  o_top0, o_rtop0, o_rbot0, o_bot0, o_lbot0, o_ltop0, o_mid0: out std_logic; --output pins for 7seg 0
+		  o_top1, o_rtop1, o_rbot1, o_bot1, o_lbot1, o_ltop1, o_mid1: out std_logic; --output pins for 7seg 1
+		  o_top2, o_rtop2, o_rbot2, o_bot2, o_lbot2, o_ltop2, o_mid2: out std_logic; --output pins for 7seg 2
+		  o_top3, o_rtop3, o_rbot3, o_bot3, o_lbot3, o_ltop3, o_mid3: out std_logic; --output pins for 7seg 3
+		  o_top4, o_rtop4, o_rbot4, o_bot4, o_lbot4, o_ltop4, o_mid4: out std_logic  --output pins for 7seg 4
 		 );
 end stop_watch;
 
@@ -40,8 +40,7 @@ architecture watch of stop_watch is
 			o_bot		: out std_logic;
 			o_lbot	: out std_logic;
 			o_ltop	: out std_logic;
-			o_mid		: out std_logic;
-			o_dp		: out std_logic
+			o_mid		: out std_logic
 			);
 	end component;
 	
@@ -85,8 +84,7 @@ begin
 					o_bot		=> o_bot0,
 					o_lbot	=> o_lbot0,
 					o_ltop	=> o_ltop0,
-					o_mid		=> o_mid0,
-					o_dp		=> o_dp0
+					o_mid		=> o_mid0
 				  );
 				  
 	sev_seg1 : sev_segs
@@ -99,8 +97,7 @@ begin
 					o_bot		=> o_bot1,
 					o_lbot	=> o_lbot1,
 					o_ltop	=> o_ltop1,
-					o_mid		=> o_mid1,
-					o_dp		=> o_dp1
+					o_mid		=> o_mid1
 				  );
 				  
 	sev_seg2 : sev_segs
@@ -113,8 +110,7 @@ begin
 					o_bot		=> o_bot2,
 					o_lbot	=> o_lbot2,
 					o_ltop	=> o_ltop2,
-					o_mid		=> o_mid2,
-					o_dp		=> o_dp2
+					o_mid		=> o_mid2
 				  );
 	
 	sev_seg3 : sev_segs
@@ -127,8 +123,7 @@ begin
 					o_bot		=> o_bot3,
 					o_lbot	=> o_lbot3,
 					o_ltop	=> o_ltop3,
-					o_mid		=> o_mid3,
-					o_dp		=> o_dp3
+					o_mid		=> o_mid3
 				  );
 	
 	sev_seg4 : sev_segs
@@ -141,8 +136,7 @@ begin
 					o_bot		=> o_bot4,
 					o_lbot	=> o_lbot4,
 					o_ltop	=> o_ltop4,
-					o_mid		=> o_mid4,
-					o_dp		=> o_dp4
+					o_mid		=> o_mid4
 				  );
 	--end seven seg declarations
 	
@@ -191,7 +185,9 @@ begin
 	--laches and unlaches the start
 	process(i_st)
 	begin
-		r_started <= not(r_started);
+		if rising_edge(i_st) then
+			r_started <= not(r_started);
+		end if;
 	end process;
 	
 end watch;
