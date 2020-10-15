48d1d739c80933bf3c7c406e1f2c9b0d39dd1303
RamblinRust <rrobertson1996@yahoo.com>
2020-10-15 01:35:05 -0400

    Update clock_tb.vhd
    updated clock testbench for the 1KHz clock

--- clock_tb.vhd
+++ clock_tb.vhd
@@ -10,27 +10,27 @@ architecture testbench of clock_tb is
 	constant c_CLOCK_PERIOD : time := 20 ns; --50MHz
 	
 	signal r_clock : std_logic := '0';
-	signal w_1Hz	: std_logic;
+	--signal w_1Hz	: std_logic;
 	signal w_1kHz	: std_logic;
-	signal w_1MHz	: std_logic;
+	--signal w_1MHz	: std_logic;
 	
-	component clock is
+	component clock_1k is
 		port(
 			  i_50MHz	: in  std_logic;
-			  o_1Hz		: out std_logic;
-			  o_1kHz		: out std_logic;
-			  o_1MHz		: out std_logic 
+			 -- o_1Hz		: out std_logic;
+			  o_1kHz		: out std_logic
+			  --o_1MHz		: out std_logic 
 			 );
 	end component;
 	
 begin
 
-	clock_Comp : clock
+	clock_Comp : clock_1k
 	port map(
 				i_50MHz	=> r_clock,
-				o_1Hz		=> w_1Hz,
-				o_1kHz	=> w_1kHz,
-				o_1MHz	=> w_1MHz
+				--o_1Hz		=> w_1Hz,
+				o_1kHz	=> w_1kHz
+				--o_1MHz	=> w_1MHz
 			  );
 
 	process
