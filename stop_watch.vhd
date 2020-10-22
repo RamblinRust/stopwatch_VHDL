c0d6a59d27a04685cafa15bf3019e29db2a55b0e
RamblinRust <rrobertson1996@yahoo.com>
2020-10-21 20:48:43 -0400

    Update stop_watch.vhd
    Changed the clock for each to be the inverse of the carry out so that it counted up when the number changes as opposed to before the number before switched from 9 to 0.

--- stop_watch.vhd
+++ stop_watch.vhd
@@ -144,8 +144,8 @@ begin
 				  
 	mod10_1 : modCounter
 		port map(
-					i_clk => w_clk,
-					i_en => w_co0,
+					i_clk => not(w_co0),
+					i_en => r_started,
 					i_clr => not(i_clr),
 					o_num => r_hundredths,
 					o_co => w_co1
@@ -153,8 +153,8 @@ begin
 				  
 	mod10_2 : modCounter
 		port map(
-					i_clk => w_clk,
-					i_en => w_co1,
+					i_clk => not(w_co1),
+					i_en => r_started,
 					i_clr => not(i_clr),
 					o_num => r_tenths,
 					o_co => w_co2
@@ -162,8 +162,8 @@ begin
 	
 	mod10_3 : modCounter
 		port map(
-					i_clk => w_clk,
-					i_en => w_co2,
+					i_clk => not(w_co2),
+					i_en => r_started,
 					i_clr => not(i_clr),
 					o_num => r_ones,
 					o_co => w_co3
@@ -171,8 +171,8 @@ begin
 				  
 	mod10_4 : modCounter
 		port map(
-					i_clk => w_clk,
-					i_en => w_co3,
+					i_clk => not(w_co3),
+					i_en => r_started,
 					i_clr => not(i_clr),
 					o_num => r_tens,
 					o_co => open
